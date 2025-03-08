158bb4ae45a3543032607031e44f95180c8f6e2b
CodeMaster1998 <oussemas410@gmail.com>
2025-03-08 01:03:33 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
 
 menuOpenButton.addEventListener("click", () => {
- document.body.classList.toggle("show-mobile-menu");
+    document.body.classList.add("show-mobile-menu");
+});
+
+menuCloseButton.addEventListener("click", () => {
+    document.body.classList.remove("show-mobile-menu");
+});
+
+// Close the menu when clicking outside of it
+document.addEventListener("click", (event) => {
+    const isClickInsideMenu = document.querySelector(".nav-menu").contains(event.target);
+    const isClickOnMenuButton = menuOpenButton.contains(event.target);
+
+    if (!isClickInsideMenu && !isClickOnMenuButton) {
+        document.body.classList.remove("show-mobile-menu");
+    }
 });
